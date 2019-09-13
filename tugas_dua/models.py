54cf6f68d6470c6f22f80282efa2325d358ed9ad
unknown <002myusuf@gmail.com>
2019-09-13 21:11:51 +0700

    Update Image model

--- tugas_dua/models.py
+++ tugas_dua/models.py
@@ -3,9 +3,6 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     image = models.ImageField(upload_to='img/', blank=True, null=True)
-
-    def __str__(self):
-        return self.image.url[len("/media/img/"):]
-
+    
     class Meta:
         verbose_name_plural = "Images"
